800bdf0f124a2ed35ce85f6450ca0f7fe5cd6cac
Niatsoop <82762504+Niatsoop@users.noreply.github.com>
2022-08-12 18:02:53 +0100

    Update bot.js

--- acidtemp/config/bot.js
+++ acidtemp/config/bot.js
@@ -14,7 +14,7 @@ module.exports = {
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
-    botadmins: ['632996702202232862', '655787293281615882', '530740430103379974', '900879623376752651', '615871946411737105', '671030008369774684','724430610923520022']
+    botadmins: ['ID1', 'ID2']
 };
 
 
